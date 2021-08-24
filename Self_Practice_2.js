58c0d2cfe834742656fb11f366a895213d7336ec
itsmeny <chokjaroen.estk@mail.kmutt.ac.th>
2021-08-24 11:29:08 +0700

    Self Practice 2

--- Self_Practice_2.js
+++ Self_Practice_2.js
@@ -18,3 +18,10 @@ console.log(typeof (num)); // 'string'
 console.log(1 + '1'); // implicit conversion (1 ตัวแรก String(1)) มันเลยได้ผลออกมาเป็น 11 เพราะเอา String มาต่อกัน
 
 console.log('5' * '4'); // implicit conversion (Number('5') , Number('4') เดาว่าถ้าเราพยายามเรียกใช้คำสั่งเกี่ยวกับคณิต คูณลบหารเลข มันจะพยายามแปลงค่าที่จะมาทำการ คูณลบหารเลข กันให้เป็นตัวเลข (20)
+
+null == undefined // == เช็คแบบ boolean คือ true , false มันเลย implicit conversion เป็น Boolean(null) == Boolean(undefined) ผลออกมาเป็น true เพราะทั้งคู่ false , false เหมือนกัน
+"0" == 0 // ได้ true แปลง String 0 เป็น Number 0
+0 == false // ได้ true แปลง false เป็น Number 0
+"0" == false // ได้ true แปลง false เป็น Number 0
+
+1 + 1 == 2 ? console.log('TRUE') : console.log('FALSE'); // short if แบบ java
