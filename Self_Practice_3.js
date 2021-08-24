bad666e1d8a8a37a8c5dc61f4477b0f3211d8f33
itsmeny <chokjaroen.estk@mail.kmutt.ac.th>
2021-08-24 12:13:41 +0700

    Self Practice 3

---
+++ Self_Practice_3.js
@@ -0,0 +1,38 @@
+// แบบฝึกหัด 1 เขียนโปรแกรมเพื่อแสดงราศีที่ตรงกับปีที่กำหนดไว้ โดยมีทั้งหมด 12 ราศี ซึ่งแทนโดยสัตว์ประเภทต่างๆ ตัวอย่างเช่น ปี 1900 % 12 จะมีค่า 4 ซึ่งจะแทนด้วยราศีหนู
+//แบบ function switch
+function get_zodiac_switch(year) {
+    switch (year % 12) {
+        case 0: return 'monkey';
+            break;
+        case 1: return 'rooster';
+            break;
+        case 2: return 'dog';
+            break;
+        case 3: return 'pig';
+            break;
+        case 4: return 'rat';
+            break;
+        case 5: return 'ox';
+            break;
+        case 6: return 'tiger';
+            break;
+        case 7: return 'rabbit';
+            break;
+        case 8: return 'dragon';
+            break;
+        case 9: return 'snake';
+            break;
+        case 10: return 'horse';
+            break;
+        case 11: return 'sheep';
+            break;
+    }
+}
+//แบบ function ที่อ้างถึง index ของ array
+function get_zodiac_array(year) {
+    const zodiac = ['monkey', 'rooster', 'dog', 'pig', 'rat', 'ox', 'tiger', 'rabbit', 'dragon', 'snake', 'horse', 'sheep'];
+    return zodiac[year % 12];
+}
+
+console.log(get_zodiac_array(2002)); // เรียกใช้ function
+console.log(get_zodiac_switch(2002)); // เรียกใช้ function
