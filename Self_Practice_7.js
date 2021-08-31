a90df8655dbd1ee05460b29c5f48b2dc6a30eebf
itsmeny <chokjaroen.estk@mail.kmutt.ac.th>
2021-08-31 22:10:11 +0700

    Self Practice 7

--- Self_Practice_7.js
+++ Self_Practice_7.js
@@ -1,12 +1,15 @@
 // แบบฝึกหัดที่ 2 เกมทายเลข 1-10
-
-let randomNumber = Math.floor(Math.random() * 10) + 1; // สุ่มเลข 1 - 10
-let guessedNumber = 2; // ใส่ค่าเลขที่เราจะเดา
-
-if (guessedNumber > randomNumber) { // ถ้าเลขที่เราเดามากกว่าเลขที่สุ่มก็
-    console.log(`Too high || The number you guessed : ${guessedNumber} || Random Number : ${randomNumber}`);
-} else if (guessedNumber < randomNumber) { // ถ้าเลขที่เราเดาน้อยกว่าเลขที่สุ่มก็
-    console.log(`Too Low || The number you guessed : ${guessedNumber} || Random Number :  ${randomNumber}`);
-} else { // ถ้าเลขที่เราเดามันตรงกับเลขที่สุ่มก็
-    console.log(`You WIN!`);
+function guessnumberGame(guessedNumber) {
+    let randomNumber = Math.floor(Math.random() * 10) + 1; // สุ่มเลข 1 - 10
+    if (guessedNumber > randomNumber) { // ถ้าเลขที่เราเดามากกว่าเลขที่สุ่มก็
+        return (`Too high || The number you guessed : ${guessedNumber} || Random Number : ${randomNumber}`);
+    } else if (guessedNumber < randomNumber) { // ถ้าเลขที่เราเดาน้อยกว่าเลขที่สุ่มก็
+        return (`Too Low || The number you guessed : ${guessedNumber} || Random Number :  ${randomNumber}`);
+    } else { // ถ้าเลขที่เราเดามันตรงกับเลขที่สุ่มก็
+        return (`You WIN!`);
+    }
 }
+
+console.log(guessnumberGame(2)); // เรียกใช้ guessnumberGame()
+console.log(guessnumberGame(3)); // เรียกใช้ guessnumberGame()
+console.log(guessnumberGame(4)); // เรียกใช้ guessnumberGame()
